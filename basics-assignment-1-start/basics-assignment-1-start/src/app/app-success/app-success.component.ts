ef8db255bf2a21d338ad14d47e4906f9fa190421
Sai Shashank Gaddam <saishashank.gaddam@stonybrook.edu>
2024-03-06 13:57:22 -0500

    feat(assignment-1): Angular Assignment 1

---
+++ basics-assignment-1-start/basics-assignment-1-start/src/app/app-success/app-success.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-success',
+  templateUrl: './app-success.component.html',
+  styleUrl: './app-success.component.css'
+})
+export class AppSuccessComponent {
+
+}
---
+++ basics-assignment-1-start/basics-assignment-1-start/src/app/app-warning/app-warning.component.ts
@@ -0,0 +1,10 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector: 'app-warning',
+    templateUrl: './app-warning.component.html',
+    styleUrls: ['./app-warning.component.css']
+})
+export class AppWarningComponent {
+
+}
